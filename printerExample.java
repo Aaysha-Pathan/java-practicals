3c2e1b0431d0fab4c9c44e914202a95769839ad3
Aaysha-Pathan <aayshaipathan@gmail.com>
2025-11-17 10:59:38 +0530

    Add files via upload

---
+++ printerExample.java
@@ -0,0 +1,37 @@
+package javaa;
+class Printer
+{
+	void print() {
+		System.out.println("Printing");	
+	}
+	void testPrint(Printer p) {
+		p.print();
+	}
+}
+class LaserPrinter extends Printer{
+	void print() {
+		System.out.println("printing from laser printer");
+	}
+	
+}
+
+class InkjetPrinter extends Printer{
+	void print() {
+		System.out.println("printing from inkjet printer");
+	}
+	
+	}
+public class printerExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Printer p=new Printer();
+		LaserPrinter lp=new LaserPrinter();
+		InkjetPrinter ip=new InkjetPrinter();
+		p.testPrint(p);  
+        p.testPrint(lp);     
+        p.testPrint(ip);
+
+	}
+
+}
